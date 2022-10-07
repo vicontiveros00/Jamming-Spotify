6ef2f00c12a16a54d38266271cd2aaa56e9d6203
Vic <vontiverosewhs@gmail.com>
2022-10-07 18:04:12 +0300

    First commit

---
+++ src/Components/SearchBar/SearchBar.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import './SearchBar.css';
+
+class SearchBar extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            term: ''
+        }
+        
+        this.search = this.search.bind(this);
+        this.handleTermChange = this.handleTermChange.bind(this);
+    }
+
+    search() {
+        this.props.onSearch(this.state.term);
+    }
+
+    handleTermChange(e) {
+        console.log(e.target.value);
+        this.setState({
+            term: e.target.value
+        })
+    }
+
+    render() {
+        return (
+            <div className="SearchBar">
+                <input placeholder="Enter A Song, Album, or Artist" onChange={this.handleTermChange} onKeyDown={(e) => {
+                    if (e.key === 'Enter') {
+                        this.search();
+                    }
+                }}/>
+                <button onClick={this.search} className="SearchButton">SEARCH</button>
+            </div>
+        )
+    }
+}
+
+export default SearchBar;
---
+++ src/util/Spotify.js
@@ -0,0 +1,77 @@
+let accessToken;
+const clientID = '';
+const redirectURI = 'http://localhost:3000/';
+const url = 'https://api.spotify.com/v1/';
+
+
+const Spotify = {
+    getAccessToken() {
+        if(accessToken) {
+            return accessToken
+        }
+        const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
+        const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
+
+        if(accessTokenMatch && expiresInMatch) {
+            accessToken = accessTokenMatch[1];
+            const expiresIn = Number(expiresInMatch[1]);
+            //clears parameters and grab new token when it expires
+            window.setTimeout(() => accessToken = '', expiresIn * 1000);
+            window.history.pushState('Access Token', null, '/');
+            return accessToken;
+        } else {
+            const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientID}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectURI}`;
+            window.location = accessUrl;
+        }
+    },
+    searchSpotify(term) {
+        return fetch(`${url}search?type=track&q=${term}`, {
+            headers: {Authorization: `Bearer ${Spotify.getAccessToken()}`}
+        }).then((response) => {
+            return response.json();
+        }).then((jsonResponse) => {
+            if(!jsonResponse.tracks) {
+                return [];
+            }
+             return jsonResponse.tracks.items.map(track => ({
+                id: track.id,
+                name: track.name,
+                artist: track.artists[0].name,
+                album: track.album.name,
+                uri: track.uri
+             }))   
+            }) 
+    },
+    savePlaylist(name, trackUris) {
+        if(!name || !trackUris.length) {
+            return;
+        }
+        const accessToken = Spotify.getAccessToken();
+        const headers = {
+            Authorization: `Bearer ${accessToken}`
+        }
+        let userId;
+
+        return fetch(`${url}me`, {headers: headers})
+        .then(response => response.json())
+        .then(jsonResponse => {
+            userId = jsonResponse.id;
+            return fetch(`${url}users/${userId}/playlists`, {
+                headers: headers,
+                method: 'POST',
+                body: JSON.stringify({ name: name })
+            }).then(response => response.json()).then(jsonResponse => {
+                const playlistId = jsonResponse.id;
+                return fetch(`${url}users/${userId}/playlists/${playlistId}/tracks`,{ 
+                    headers: headers,
+                    method: 'POST',
+                    body: JSON.stringify({
+                        uris: trackUris 
+                    })
+                })
+            })
+        })
+    }
+}
+
+export default Spotify;
